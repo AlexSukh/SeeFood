ec34f1d660e6db1c9777620af43e820d52cab06d
Alex Sukhitashvili <sukhitashvilialex2@gmail.com>
2020-11-12 21:12:10 +0400

    feat: feed an image into ML model

--- SeeFood/ViewController.swift
+++ SeeFood/ViewController.swift
@@ -31,9 +31,48 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         if let userPickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage{
             
             imageView.image = userPickedImage
+            
+            guard let ciimage = CIImage(image: userPickedImage) else {
+                fatalError("Could not convert to ci image")
+            }
+            
+            detect(image: ciimage)
         }
         
         imagePicker.dismiss(animated: true, completion: nil)
+        
+        
+    }
+    
+    func detect(image: CIImage) {
+        
+        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else {
+            fatalError("Loading COreML Model Failed.")
+            
+        }
+        
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            guard let result = request.results as? [VNClassificationObservation] else {
+                fatalError("Model failed to process image")
+            }
+            
+            print(result)
+            
+            
+            
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: image)
+        
+        do {
+            try handler.perform([request])
+
+        } catch {
+            print(error)
+            
+        }
+        
+        
         
         
     }
