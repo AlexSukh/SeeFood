10d84ed9cfeae23d60cdb95aa1a5a8c34b1b97b5
Alex Sukhitashvili <sukhitashvilialex2@gmail.com>
2020-11-12 20:03:42 +0400

    Initial Commit

---
+++ SeeFood/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeeFood
+//
+//  Created by Alex Sukhitashvili on 11/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
